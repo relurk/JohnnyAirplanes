9118bee602fd9299870a796874ed84127e1cfe64
Jakob peterson <jaykob.g1@gmail.com>
2015-01-08 01:11:25 -0700

    splash screen concept

--- gamestate/menu.lua
+++ gamestate/menu.lua
@@ -9,5 +9,6 @@ function gsMenu.update()
 end
 
 function gsMenu.draw()
+	love.graphics.setBackgroundColor(0,0,0)
 	love.graphics.print("Menu state initiated!", 20, 20)
 end
--- gamestate/new.lua
+++ gamestate/new.lua
@@ -1,4 +1,8 @@
 gsNew = {}
+splash1 = { "This is wonderful" }
+splash2 = { "THIS IS WONDERFUL" }
+
+
 
 function gsNew.load()
 
@@ -9,5 +13,8 @@ function gsNew.update()
 end
 
 function gsNew.draw()
+	love.graphics.setBackgroundColor(50,50,100)
 	love.graphics.print("New gamestate ! press 2 for more", 20, 20)
+	love.timer.sleep( 2 )
+	state.set("menu")
 end
